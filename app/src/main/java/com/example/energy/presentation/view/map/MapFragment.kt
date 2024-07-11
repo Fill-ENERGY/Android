ba2c07f9565d8d28c3df2c32e939457ad1ba1a38
gitsuhyun <suhyun10201@naver.com>
2024-07-11 22:19:06 +0900

    [Feat] #3 sos 기능 추가

--- app/src/main/java/com/example/energy/presentation/view/map/MapFragment.kt
+++ app/src/main/java/com/example/energy/presentation/view/map/MapFragment.kt
@@ -1,5 +1,15 @@
 package com.example.energy.presentation.view.map
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,13 +17,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.energy.R
 import com.example.energy.databinding.FragmentMapBinding
+import com.example.energy.presentation.view.MainActivity
 import com.example.energy.presentation.view.base.BaseFragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import com.kakao.vectormap.camera.CameraUpdate
+import com.kakao.vectormap.camera.CameraUpdateFactory
+import com.kakao.vectormap.label.LabelLayer
+import com.kakao.vectormap.label.LabelOptions
 
 
 class MapFragment : BaseFragment<FragmentMapBinding>({ FragmentMapBinding.inflate(it)}) {
@@ -22,6 +42,7 @@ class MapFragment : BaseFragment<FragmentMapBinding>({ FragmentMapBinding.inflat
         super.onViewCreated(view, savedInstanceState)
 
         val mapView : MapView = binding.mapView
+
         mapView.start(object : MapLifeCycleCallback() {
 
 
@@ -38,10 +59,18 @@ class MapFragment : BaseFragment<FragmentMapBinding>({ FragmentMapBinding.inflat
         }, object : KakaoMapReadyCallback() {
             override fun onMapReady(kakaoMap: KakaoMap) {
                 // 인증 후 API 가 정상적으로 실행될 때 호출됨
+
             }
         })
 
+        //sos
+        binding.ivSos.setOnClickListener {
+            var intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:112")
 
+            startActivity(intent)
+        }
 
     }
+
 }
