671e59378550324be715db5fd9182f42561fe95c
znayeonzn <wndkdus102@naver.com>
2024-07-15 15:21:59 +0900

    [Feat] 리스트 목록 기능 구현

---
+++ app/src/main/java/com/example/energy/presentation/view/list/listdata.kt
@@ -0,0 +1,8 @@
+package com.example.energy.presentation.view.list
+
+data class listdata(
+    val location_name : String,
+    val distance : String,
+    val grade : String,
+    val time : String
+)
