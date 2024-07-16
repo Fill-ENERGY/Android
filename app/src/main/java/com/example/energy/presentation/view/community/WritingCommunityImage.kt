ef8043cebd63c205f165a683fdd77a3632f32d79
serena <tjgusid@naver.com>
2024-07-17 02:46:27 +0900

    [Feat] 업로드 할 이미지 중 '대표'이미지 라벨 추가

--- app/src/main/java/com/example/energy/presentation/view/community/WritingCommunityImage.kt
+++ app/src/main/java/com/example/energy/presentation/view/community/WritingCommunityImage.kt
@@ -3,5 +3,6 @@ package com.example.energy.presentation.view.community
 import android.net.Uri
 
 data class WritingCommunityImage(
-    val imageUrl: Uri // 선택한 이미지
+    val imageUrl: Uri, // 선택한 이미지
+    var isRepresentative: Boolean = false, // 대표 이미지 여부를 나타내는 속성 추가
 )
