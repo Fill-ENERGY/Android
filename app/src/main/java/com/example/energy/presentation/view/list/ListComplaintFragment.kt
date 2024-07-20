af1153501369db2671c9fd28ae659333f9f0a017
gitsuhyun <suhyun10201@naver.com>
2024-07-20 22:08:18 +0900

    [Feat] #3 민원 상세 페이지 구현

---
+++ app/src/main/java/com/example/energy/presentation/view/list/ListComplaintFragment.kt
@@ -0,0 +1,8 @@
+package com.example.energy.presentation.view.list
+
+import com.example.energy.databinding.FragmentListComplaintBinding
+import com.example.energy.presentation.view.base.BaseFragment
+
+class ListComplaintFragment : BaseFragment<FragmentListComplaintBinding>({ FragmentListComplaintBinding.inflate(it) }) {
+
+}
