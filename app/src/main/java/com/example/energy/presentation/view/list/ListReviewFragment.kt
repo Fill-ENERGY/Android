39eb804476175468369abeb1a0b1020a9c9ac7d7
gitsuhyun <suhyun10201@naver.com>
2024-07-20 21:14:46 +0900

    [Feat] #3 평가 상세 페이지 구현

---
+++ app/src/main/java/com/example/energy/presentation/view/list/ListReviewFragment.kt
@@ -0,0 +1,10 @@
+package com.example.energy.presentation.view.list
+
+import androidx.fragment.app.Fragment
+import com.example.energy.databinding.FragmentListReviewBinding
+import com.example.energy.databinding.FragmentMapBinding
+import com.example.energy.presentation.view.base.BaseFragment
+
+class ListReviewFragment : BaseFragment<FragmentListReviewBinding>({ FragmentListReviewBinding.inflate(it) }) {
+
+}
