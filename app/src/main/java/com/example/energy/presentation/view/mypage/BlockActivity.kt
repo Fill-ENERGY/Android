39ad47a984e089d09e2d98aeb0d5d73687d07037
gitsuhyun <suhyun10201@naver.com>
2024-07-22 19:03:47 +0900

    [Feat] #21 차단 목록 ui 추가

---
+++ app/src/main/java/com/example/energy/presentation/view/mypage/BlockActivity.kt
@@ -0,0 +1,9 @@
+package com.example.energy.presentation.view.mypage
+
+import com.example.energy.databinding.ActivityBlockBinding
+import com.example.energy.presentation.view.base.BaseActivity
+
+class BlockActivity : BaseActivity<ActivityBlockBinding>({ ActivityBlockBinding.inflate(it)}) {
+
+
+}
