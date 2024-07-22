9efa2f83dacbed9b3181becd37e75d330eaf9caf
serena <tjgusid@naver.com>
2024-07-23 02:53:13 +0900

    [Feat] 커뮤니티 카테고리 선택 기능

--- app/src/main/java/com/example/energy/presentation/view/community/CommunityWritingActivity.kt
+++ app/src/main/java/com/example/energy/presentation/view/community/CommunityWritingActivity.kt
@@ -6,34 +6,81 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.energy.R
 import com.example.energy.data.repository.community.WritingCommunityImage
 import com.example.energy.databinding.ActivityCommunityWritingBinding
-import com.example.energy.databinding.DialogSelectCategoryBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
-
 
 class CommunityWritingActivity : AppCompatActivity(), GalleryAdapter.MyItemClickListener {
     private lateinit var binding: ActivityCommunityWritingBinding
-    var imageList = ArrayList<WritingCommunityImage>() //선택한 이미지 데이터 리스트
-    val adapter = GalleryAdapter(imageList) //Recycler View Adapter
+    var imageList = ArrayList<WritingCommunityImage>() // 선택한 이미지 데이터 리스트
+    val adapter = GalleryAdapter(imageList) // Recycler View Adapter
+    val initText = "카테고리를 선택해 주세요"
+    private val menuList = listOf("카테고리를 선택해 주세요", "일상","궁금해요","도와줘요","휠체어", "스쿠터")
+
+    private lateinit var spinner: Spinner
+    private lateinit var titleEditText: EditText
+    private lateinit var contentEditText: EditText
+    private lateinit var finishButton: TextView
+    private var isCategorySelected = false
+    private var isTitleFilled = false
+    private var isContentFilled = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCommunityWritingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Recycler View & Adapter 연결
+        spinner = binding.communitySelectCategory
+        titleEditText = binding.communityWritingTitleTv
+        contentEditText = binding.communityWritingContent
+        finishButton = binding.communityWritingFinishTv
+
+        // 제목 EditText 변화 listener
+        titleEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                isTitleFilled = !s.isNullOrEmpty()
+                updateFinishButtonState()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+        // 내용 EditText 변화 listener
+        contentEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                isContentFilled = !s.isNullOrEmpty()
+                updateFinishButtonState()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        // 사진 Recycler View & Adapter 연결
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         binding.selectedImagesContainer.layoutManager = layoutManager
         binding.selectedImagesContainer.adapter = adapter
 
-        // 어댑터에 클릭 리스너 설정
+        // 사진 어댑터에 클릭 리스너 설정
         adapter.setMyItemClickListener(this)
 
         // 뒤로가기 버튼
@@ -41,48 +88,84 @@ class CommunityWritingActivity : AppCompatActivity(), GalleryAdapter.MyItemClick
             finish() // 현재 액티비티 종료
         }
 
-        // 카테고리 선택 버튼 클릭 리스너 설정
-        binding.communityWritingCategoryUnderBtn.setOnClickListener {
-            showCategoryDialog()
+        // Spinner Adapter 연결
+        val spinnerAdapter = OptionSpinnerAdapter(this, R.layout.item_select_category, menuList, initText)
+        binding.communitySelectCategory.setAdapter(spinnerAdapter)
+        binding.communitySelectCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                spinnerAdapter.setSelectedPosition(position) // 선택한 항목 설정
+                isCategorySelected = true
+                updateFinishButtonState()
+
+                val value = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@CommunityWritingActivity, value, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // 선택되지 않은 경우 처리
+                isCategorySelected = false
+                updateFinishButtonState()
+            }
+        }
+
+        // 등록 버튼 click listener
+        finishButton.setOnClickListener {
+            if (finishButton.isEnabled) {
+                finish()
+            }
         }
 
         // 이미지 업로드 사진 클릭 시
         binding.communityWritingImageSelect.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK) //갤러리 호출
+            val intent = Intent(Intent.ACTION_PICK) // 갤러리 호출
             intent.type = "image/*"
-            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true) //멀티 선택 기능
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true) // 멀티 선택 기능
             activityResult.launch(intent)
         }
-    }
-    private fun showCategoryDialog() { //카테고리 선택 화면
-        val dialog = Dialog(this)
-        val binding = DialogSelectCategoryBinding.inflate(layoutInflater)
-        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 기존 다이어그램 배경 투명으로 적용(커스텀한 배경이 보이게 하기 위함)
-        dialog.window?.setLayout(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        dialog.setContentView(binding.root)
-        dialog.setCancelable(true) // 바깥 영역 터치하면 닫힘
 
-        // 다이얼로그 표시
-        dialog.show()
+        // 키보드 외부 화면 클릭 시 키보드 숨기기
+        binding.activityCommunityWriting.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboard()
+            }
+            false
+        }
     }
 
-    // 이미지 가져오기
+//    // CommunityFragment로 이동하는 함수
+//    private fun navigateToCommunityFragment() {
+//        val communityFragment = CommunityFragment()
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.main_frm, communityFragment) // Replace with your fragment container ID
+//            .addToBackStack(null) // Add to back stack if you want to allow the user to return to the previous screen
+//            .commit()
+//    }
+
+    // 등록 버튼 상태 업데이트하는 함수
+    private fun updateFinishButtonState() {
+        finishButton.isEnabled = isTitleFilled && isContentFilled && isCategorySelected
+        if (finishButton.isEnabled) {
+            finishButton.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.gray_scale8))
+        } else {
+            finishButton.setTextColor(ContextCompat.getColor(applicationContext!!, R.color.gray_scale5))
+        }
+
+    }
+
+    // 이미지 가져오는 함수
     private var activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ){
-        //결과 코드 OK, 결과값 null 아니면 (이미지를 선택하면..)
-        if (it.resultCode == RESULT_OK){
-            //멀티 선택은 clip Data
-            if (it.data!!.clipData != null) { //멀티 이미지
-                val count = it.data!!.clipData!!.itemCount //선택한 이미지 갯수
-                for(index in 0 until count){
-                    val imageUri = it.data!!.clipData!!.getItemAt(index).uri //이미지 담기
-                    addImageToList(imageUri) //이미지 추가
+    ) {
+        // 결과 코드 OK, 결과값 null 아니면 (이미지를 선택하면..)
+        if (it.resultCode == RESULT_OK) {
+            // 멀티 선택은 clip Data
+            if (it.data!!.clipData != null) { // 멀티 이미지
+                val count = it.data!!.clipData!!.itemCount // 선택한 이미지 갯수
+                for (index in 0 until count) {
+                    val imageUri = it.data!!.clipData!!.getItemAt(index).uri // 이미지 담기
+                    addImageToList(imageUri) // 이미지 추가
                 }
-            } else{ //싱글 이미지
+            } else { // 싱글 이미지
                 val imageUri = it.data!!.data
                 addImageToList(imageUri!!)
             }
@@ -90,13 +173,15 @@ class CommunityWritingActivity : AppCompatActivity(), GalleryAdapter.MyItemClick
         }
     }
 
-    private fun addImageToList(imageUri: Uri) { //데이터 리스트에 업로드하는 이미지 저장
+    // 데이터 리스트에 업로드하는 이미지 저장
+    private fun addImageToList(imageUri: Uri) {
         val isRepresentative = imageList.isEmpty() // 첫 번째 이미지인 경우 대표 이미지로 설정
         imageList.add(WritingCommunityImage(imageUri, isRepresentative))
         adapter.notifyItemInserted(imageList.size - 1)
     }
 
-    override fun onRemoveImage(position: Int) { //이미지 삭제
+    // 이미지 삭제
+    override fun onRemoveImage(position: Int) {
         adapter.removeImage(position)
         // 대표 이미지가 삭제된 경우 새로운 대표 이미지 설정
         if (imageList.isNotEmpty() && !imageList.any { it.isRepresentative }) {
@@ -105,4 +190,15 @@ class CommunityWritingActivity : AppCompatActivity(), GalleryAdapter.MyItemClick
         }
     }
 
+    // 키보드 내리는 함수
+    private fun hideKeyboard() {
+        val currentFocusView = currentFocus
+        if (currentFocusView != null) {
+            val inputManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputManager.hideSoftInputFromWindow(
+                currentFocusView.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
+        }
+    }
 }
