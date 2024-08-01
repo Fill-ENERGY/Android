7642a7b00e33e5a5c691adbe593342e03ceff9cb
znayeonzn <wndkdus102@naver.com>
2024-08-01 18:13:23 +0900

    [Feat] 스와이프 기능 연결

--- app/src/main/java/com/example/energy/presentation/view/note/NoteFragment.kt
+++ app/src/main/java/com/example/energy/presentation/view/note/NoteFragment.kt
@@ -2,35 +2,31 @@ package com.example.energy.presentation.view.note
 
 import android.content.Intent
 import android.graphics.Color
-import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.energy.R
 import com.example.energy.databinding.DialogCustomBinding
 import com.example.energy.databinding.FragmentNoteBinding
-import com.example.energy.databinding.FragmentNoteListBinding
 import com.example.energy.presentation.view.base.BaseFragment
-import com.example.energy.presentation.view.list.CustomDividerItemDecoration
-import com.example.energy.presentation.view.mypage.BlockActivity
-import com.kakao.vectormap.mapwidget.component.Vertical
+
 
 class NoteFragment : BaseFragment<FragmentNoteBinding>({ FragmentNoteBinding.inflate(it)}) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
         setToolBar()
 
         // 테스트 데이터
-        val sampleData = listOf(
+        val sampleData = arrayListOf(
             NoteItem("김규리", "user123", "그럼 조그만 기다리세요!", "2분 전 "),
             NoteItem("박지민", "user456", "네 감사합니다.!", "어제")
 
@@ -40,8 +36,21 @@ class NoteFragment : BaseFragment<FragmentNoteBinding>({ FragmentNoteBinding.inf
         val noteAdapter = NoteAdapter(sampleData)
 
 
+
+
+
+
+
         // 채팅 구분선
         val Itemdeco = DividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
+
+
+        //스와이프 삭제 리사이클러뷰와 연결
+        val swipeHelper = SwipeHelper().apply{
+            setClamp(200f)
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeHelper)
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
 
 
         //채팅 목록
@@ -51,7 +60,21 @@ class NoteFragment : BaseFragment<FragmentNoteBinding>({ FragmentNoteBinding.inf
             addItemDecoration(Itemdeco)
 
 
+            //다른 채팅 목록을 클릭하면 스와이프 해제
+            setOnTouchListener { v, event ->
+                swipeHelper.removePreviousClamp(this)
+                v.performClick()
+                false
+            }
+
+
+
+
+
+
         }
+
+
 
 
 
