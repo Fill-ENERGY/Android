f265d2154d328d740b5aa18f0792bd51df789b12
gitsuhyun <suhyun10201@naver.com>
2024-08-16 00:34:22 +0900

    [Feat] #3 리뷰 ui

---
+++ app/src/main/java/com/example/energy/presentation/view/list/ListAddReviewFragment.kt
@@ -0,0 +1,8 @@
+package com.example.energy.presentation.view.list
+
+import com.example.energy.databinding.FragmentListAddReviewBinding
+import com.example.energy.presentation.view.base.BaseFragment
+
+class ListAddReviewFragment : BaseFragment<FragmentListAddReviewBinding>({ FragmentListAddReviewBinding.inflate(it)}) {
+
+}
