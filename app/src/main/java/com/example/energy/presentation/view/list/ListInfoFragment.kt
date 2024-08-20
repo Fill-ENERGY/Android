3c55be25e61bd4525932b5e2ac218e1b5f44c4a1
znayeonzn <wndkdus102@naver.com>
2024-08-21 00:45:47 +0900

    [Feat] 전체 리스트 api 수정

--- app/src/main/java/com/example/energy/presentation/view/list/ListInfoFragment.kt
+++ app/src/main/java/com/example/energy/presentation/view/list/ListInfoFragment.kt
@@ -112,7 +112,7 @@ class ListInfoFragment : BaseFragment<FragmentListInfoBinding>({ FragmentListInf
 
         if (stationId != -1) {
 
-            var accessToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imtpaml3aTFAbmF2ZXIuY29tIiwiaWF0IjoxNzIzODE3OTA5LCJleHAiOjE3MjY0MDk5MDl9.D8cHYgTwnv-k3GdJpSexakAnn7rtZvML1cfkGm9qJoY"
+            var accessToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InduZGtkdXMxMDJAbmF2ZXIuY29tIiwiaWF0IjoxNzI0MTY0NTU2LCJleHAiOjE3MjY3NTY1NTZ9.xRtumUjlAyuRhf7Ldu_7kH52XBFzqdaP6nTy0OjfvuQ"
 
 
             // 충전소 상세 정보 api 연결
